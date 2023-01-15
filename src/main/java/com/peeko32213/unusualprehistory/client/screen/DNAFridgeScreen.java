96e5140793d0609c323d9cb0e4fbcfd1227fe539
Scouter456 <markveenstra14@gmail.com>
2023-01-15 23:17:51 +0100

    Update DNAFridgeScreen.java

--- src/main/java/com/peeko32213/unusualprehistory/client/screen/DNAFridgeScreen.java
+++ src/main/java/com/peeko32213/unusualprehistory/client/screen/DNAFridgeScreen.java
@@ -10,6 +10,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 
+//Vanilla copy
 public class DNAFridgeScreen extends AbstractContainerScreen<DNAFridgeMenu> implements MenuAccess<DNAFridgeMenu> {
 private static final ResourceLocation CONTAINER_BACKGROUND = new ResourceLocation("textures/gui/container/generic_54.png");
 private final int containerRows;
