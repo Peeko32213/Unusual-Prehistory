f334b15f58a281ae66d429df1f1d0e5409f049c3
Scouter456 <markveenstra14@gmail.com>
2023-02-17 17:26:14 +0100

    Fixed crash because handler wasnt added to networking messages

--- src/main/java/com/peeko32213/unusualprehistory/common/networking/UPMessages.java
+++ src/main/java/com/peeko32213/unusualprehistory/common/networking/UPMessages.java
@@ -4,7 +4,6 @@ import com.peeko32213.unusualprehistory.UnusualPrehistory;
 import com.peeko32213.unusualprehistory.common.networking.packet.SyncItemStackC2SPacket;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.item.Item;
 import net.minecraftforge.network.NetworkDirection;
 import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.PacketDistributor;
