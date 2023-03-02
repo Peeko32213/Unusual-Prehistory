e8d3a0a41bd7a328beb4f815747d970f05a7f9d4
Scouter456 <markveenstra14@gmail.com>
2023-03-02 21:15:47 +0100

    Changed configuredfeatures and placedfeatures to be compatible with datagen

--- src/main/java/com/peeko32213/unusualprehistory/core/registry/UPConfiguredFeatures.java
+++ src/main/java/com/peeko32213/unusualprehistory/core/registry/UPConfiguredFeatures.java
@@ -1,35 +1,65 @@
 package com.peeko32213.unusualprehistory.core.registry;
 
 import com.peeko32213.unusualprehistory.UnusualPrehistory;
+import com.peeko32213.unusualprehistory.common.world.feature.tree.GinkgoFoliagePlacer;
 import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
 import net.minecraft.data.BuiltinRegistries;
+import net.minecraft.data.worldgen.features.FeatureUtils;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
+import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
+import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
+import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
 import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import org.checkerframework.checker.units.qual.C;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
 
 public class UPConfiguredFeatures {
 
-    public static void init() {
-    }
+    public static List<String> configuredFeatureList = new ArrayList<>();
+    public static final DeferredRegister<ConfiguredFeature<?,?>> CONFIGURED_FEATURES = DeferredRegister.create(Registry.CONFIGURED_FEATURE_REGISTRY, UnusualPrehistory.MODID);
 
-    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> STONE_FOSSIL_ORE = registerConfiguredFeature("stone_fossil_ore", Feature.ORE, new OreConfiguration(List.of(OreConfiguration.target(new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES), UPBlocks.STONE_FOSSIL.get().defaultBlockState()), OreConfiguration.target(new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES), UPBlocks.STONE_FOSSIL.get().defaultBlockState())), 9));
-    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> AMBER_FOSSIL_ORE = registerConfiguredFeature("amber_fossil_ore", Feature.ORE, new OreConfiguration(List.of(OreConfiguration.target(new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES), UPBlocks.STONE_AMBER_FOSSIL.get().defaultBlockState()), OreConfiguration.target(new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES), UPBlocks.STONE_AMBER_FOSSIL.get().defaultBlockState())), 5));
-    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> DEEPSLATE_FOSSIL_ORE = registerConfiguredFeature("deepslate_fossil_ore", Feature.ORE, new OreConfiguration(List.of(OreConfiguration.target(new TagMatchTest(BlockTags.DEEPSLATE_ORE_REPLACEABLES), UPBlocks.DEEPSLATE_FOSSIL.get().defaultBlockState()), OreConfiguration.target(new TagMatchTest(BlockTags.DEEPSLATE_ORE_REPLACEABLES), UPBlocks.DEEPSLATE_FOSSIL.get().defaultBlockState())), 4));
-    public static final Holder<ConfiguredFeature<OreConfiguration, ?>> PLANT_FOSSIL_ORE = registerConfiguredFeature("plant_fossil_ore", Feature.ORE, new OreConfiguration(List.of(OreConfiguration.target(new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES), UPBlocks.PLANT_FOSSIL.get().defaultBlockState()), OreConfiguration.target(new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES), UPBlocks.PLANT_FOSSIL.get().defaultBlockState())), 11));
 
-    public static <FC extends FeatureConfiguration, F extends Feature<FC>> Holder<ConfiguredFeature<FC, ?>> registerConfiguredFeature(String id, F feature, FC featureConfiguration) {
-        ResourceLocation resourceLocation = new ResourceLocation(UnusualPrehistory.MODID, id);
+    public static final RegistryObject<ConfiguredFeature<?, ?>> STONE_FOSSIL_ORE = registerConfiguredFeature("stone_fossil_ore", () -> new ConfiguredFeature<>( Feature.ORE, new OreConfiguration(List.of(OreConfiguration.target(new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES), UPBlocks.STONE_FOSSIL.get().defaultBlockState()), OreConfiguration.target(new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES), UPBlocks.STONE_FOSSIL.get().defaultBlockState())), 9)));
+    public static final RegistryObject<ConfiguredFeature<?, ?>> AMBER_FOSSIL_ORE = registerConfiguredFeature("amber_fossil_ore",  () -> new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(List.of(OreConfiguration.target(new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES), UPBlocks.STONE_AMBER_FOSSIL.get().defaultBlockState()), OreConfiguration.target(new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES), UPBlocks.STONE_AMBER_FOSSIL.get().defaultBlockState())), 5)));
+    public static final RegistryObject<ConfiguredFeature<?, ?>> DEEPSLATE_FOSSIL_ORE = registerConfiguredFeature("deepslate_fossil_ore", () -> new ConfiguredFeature<>(Feature.ORE, new OreConfiguration(List.of(OreConfiguration.target(new TagMatchTest(BlockTags.DEEPSLATE_ORE_REPLACEABLES), UPBlocks.DEEPSLATE_FOSSIL.get().defaultBlockState()), OreConfiguration.target(new TagMatchTest(BlockTags.DEEPSLATE_ORE_REPLACEABLES), UPBlocks.DEEPSLATE_FOSSIL.get().defaultBlockState())), 4)));
+    public static final RegistryObject<ConfiguredFeature<?, ?>> PLANT_FOSSIL_ORE = registerConfiguredFeature("plant_fossil_ore",() -> new ConfiguredFeature<>( Feature.ORE, new OreConfiguration(List.of(OreConfiguration.target(new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES), UPBlocks.PLANT_FOSSIL.get().defaultBlockState()), OreConfiguration.target(new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES), UPBlocks.PLANT_FOSSIL.get().defaultBlockState())), 11)));
 
-        if (BuiltinRegistries.CONFIGURED_FEATURE.keySet().contains(resourceLocation))
-            throw new IllegalStateException("Placed Feature ID: \"" + resourceLocation + "\" already exists in the Placed Features registry!");
+    public static final RegistryObject<ConfiguredFeature<?, ?>> GINKGO_TREE =
+            registerConfiguredFeature("ginkgo", () -> new ConfiguredFeature<>(Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(
+                    BlockStateProvider.simple(UPBlocks.GINKGO_LOG.get()),
+                    new StraightTrunkPlacer(7, 5, 0),
+                    BlockStateProvider.simple(UPBlocks.GINKGO_LEAVES.get()),
+                    new GinkgoFoliagePlacer( 2F, 6F, ConstantInt.of(0), 1, 0, 0.5f, 2),
+                    new TwoLayersFeatureSize(1, 1, 1)).build()));
 
-        return BuiltinRegistries.registerExact(BuiltinRegistries.CONFIGURED_FEATURE, resourceLocation.toString(), new ConfiguredFeature<>(feature, featureConfiguration));
+
+   //public static <FC extends FeatureConfiguration, F extends Feature<FC>> Holder<ConfiguredFeature<FC, ?>> registerConfiguredFeature(String id, F feature, FC featureConfiguration) {
+   //    ResourceLocation resourceLocation = new ResourceLocation(UnusualPrehistory.MODID, id);
+
+   //    if (BuiltinRegistries.CONFIGURED_FEATURE.keySet().contains(resourceLocation)) {
+   //        throw new IllegalStateException("Placed Feature ID: \"" + resourceLocation + "\" already exists in the Placed Features registry!");
+   //    }
+
+   //    configuredFeatureList.add(id);
+   //    return BuiltinRegistries.registerExact(BuiltinRegistries.CONFIGURED_FEATURE, resourceLocation.toString(), new ConfiguredFeature<>(feature, featureConfiguration));
+   //}
+
+    public static RegistryObject<ConfiguredFeature<?, ?>> registerConfiguredFeature(String name, Supplier<ConfiguredFeature<?, ?>> feature) {
+        configuredFeatureList.add(name);
+        return CONFIGURED_FEATURES.register(name, feature);
     }
 
 }
--- src/main/java/com/peeko32213/unusualprehistory/datagen/DataGenerators.java
+++ src/main/java/com/peeko32213/unusualprehistory/datagen/DataGenerators.java
@@ -1,18 +1,34 @@
 package com.peeko32213.unusualprehistory.datagen;
 
 import com.google.common.collect.Sets;
+import com.google.gson.JsonElement;
+import com.mojang.serialization.JsonOps;
 import com.peeko32213.unusualprehistory.UnusualPrehistory;
+import com.peeko32213.unusualprehistory.common.world.feature.UPPlacedFeatures;
+import com.peeko32213.unusualprehistory.core.registry.UPConfiguredFeatures;
+import net.minecraft.core.Registry;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.data.DataProvider;
 import net.minecraft.data.models.blockstates.BlockStateGenerator;
+import net.minecraft.resources.RegistryOps;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.common.data.JsonCodecProvider;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
 
-@Mod.EventBusSubscriber(modid = UnusualPrehistory.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+import static com.peeko32213.unusualprehistory.UnusualPrehistory.MODID;
+
+@Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 
 public class DataGenerators {
 
@@ -43,5 +59,41 @@ public class DataGenerators {
         generator.addProvider(true,new ItemTagsGenerator(generator, blockTags, helper));
         generator.addProvider(true,new ItemModelGenerator(generator, helper));
         generator.addProvider(true,new LanguageGenerator(generator));
+
+
+
+        final RegistryAccess registries = RegistryAccess.builtinCopy();
+        final RegistryOps<JsonElement> ops = RegistryOps.create(JsonOps.INSTANCE, registries);
+        final DataProvider configuredFeatureProvider = JsonCodecProvider.forDatapackRegistry(generator, helper, MODID, ops, Registry.CONFIGURED_FEATURE_REGISTRY, getConfiguredFeatures(registries));
+        generator.addProvider(true, configuredFeatureProvider);
+
+        final DataProvider placedFeatureProvider = JsonCodecProvider.forDatapackRegistry(generator, helper, MODID, ops, Registry.PLACED_FEATURE_REGISTRY, getPlacedFeatures(registries));
+        generator.addProvider(true, placedFeatureProvider);
+    }
+
+    public static Map<ResourceLocation, ConfiguredFeature<?, ?>> getConfiguredFeatures(RegistryAccess registries) {
+        Map<ResourceLocation, ConfiguredFeature<?, ?>> map = new HashMap<>();
+
+        for (int i = 0; i < UPConfiguredFeatures.configuredFeatureList.size(); i++) {
+            ResourceLocation RL = new ResourceLocation(MODID, UPConfiguredFeatures.configuredFeatureList.get(i));
+            Registry<ConfiguredFeature<?, ?>> configuredFeatures = registries.registryOrThrow(Registry.CONFIGURED_FEATURE_REGISTRY);
+            ConfiguredFeature<?, ?> PF = configuredFeatures.get(RL);
+            map.put(RL, PF);
+        }
+
+        return map;
+    }
+
+    public static Map<ResourceLocation, PlacedFeature> getPlacedFeatures(RegistryAccess registries) {
+        Map<ResourceLocation, PlacedFeature> map = new HashMap<>();
+
+        for (int i = 0; i < UPPlacedFeatures.placedFeatureList.size(); i++) {
+            ResourceLocation RL = new ResourceLocation(MODID, UPPlacedFeatures.placedFeatureList.get(i));
+            Registry<PlacedFeature> placedFeatures = registries.registryOrThrow(Registry.PLACED_FEATURE_REGISTRY);
+            PlacedFeature PF = placedFeatures.get(RL);
+            map.put(RL, PF);
+        }
+
+        return map;
     }
 }
