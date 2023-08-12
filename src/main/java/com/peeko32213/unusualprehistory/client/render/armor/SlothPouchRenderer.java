3c492e68ccd85a43afa5f3a23567a14db4cffcd0
Peeko32213 <peeko32213@gmail.com>
2023-08-12 16:50:38 -0700

    Minor slot changes

--- src/main/java/com/peeko32213/unusualprehistory/client/render/armor/SlothPouchRenderer.java
+++ src/main/java/com/peeko32213/unusualprehistory/client/render/armor/SlothPouchRenderer.java
@@ -1,9 +1,7 @@
 package com.peeko32213.unusualprehistory.client.render.armor;
 
 import com.peeko32213.unusualprehistory.client.model.armor.SlothPouchModel;
-import com.peeko32213.unusualprehistory.client.model.armor.TyrantsCrownModel;
 import com.peeko32213.unusualprehistory.common.item.armor.ItemSlothPouch;
-import com.peeko32213.unusualprehistory.common.item.armor.ItemTyrantsCrown;
 import software.bernie.geckolib3.renderers.geo.GeoArmorRenderer;
 
 public class SlothPouchRenderer extends GeoArmorRenderer<ItemSlothPouch> {
