d268ff86ba808e327e9e165bedfaeca2dd60941e
Scouter456 <markveenstra14@gmail.com>
2023-10-20 07:36:24 +0200

    Fix book snake

--- src/main/java/com/peeko32213/unusualprehistory/common/entity/msc/util/EntityBookSnake.java
+++ src/main/java/com/peeko32213/unusualprehistory/common/entity/msc/util/EntityBookSnake.java
@@ -46,6 +46,7 @@ import software.bernie.geckolib3.core.manager.AnimationFactory;
 import software.bernie.geckolib3.util.GeckoLibUtil;
 
 import javax.annotation.Nullable;
+import java.util.Collections;
 
 public class EntityBookSnake extends LivingEntity implements IAnimatable, IBookEntity {
     private static final EntityDataAccessor<Boolean> FROM_BOOK = SynchedEntityData.defineId(EntityBookSnake.class, EntityDataSerializers.BOOLEAN);
@@ -69,12 +70,12 @@ public class EntityBookSnake extends LivingEntity implements IAnimatable, IBookE
 
     @Override
     public Iterable<ItemStack> getArmorSlots() {
-        return null;
+        return Collections.emptyList();
     }
 
     @Override
     public ItemStack getItemBySlot(EquipmentSlot pSlot) {
-        return null;
+        return ItemStack.EMPTY;
     }
 
     @Override
@@ -84,7 +85,7 @@ public class EntityBookSnake extends LivingEntity implements IAnimatable, IBookE
 
     @Override
     public HumanoidArm getMainArm() {
-        return null;
+        return HumanoidArm.RIGHT;
     }
 
 
