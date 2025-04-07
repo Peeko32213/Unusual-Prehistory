f3a9f9fa2d6560da178addee740cfe869c775d47
platypushasnohat <58866828+PlatypusOfHats@users.noreply.github.com>
2025-04-06 22:57:15 -0600

    woodland mansion piece test
    - basically another place big skeletons can spawn, might need to add something to mansion class to spawn the skeletons

---
+++ src/main/java/com/peeko32213/unusualprehistory/mixin/WoodlandMansionPiecesMixin.java
@@ -0,0 +1,9 @@
+//package com.peeko32213.unusualprehistory.mixin;
+//
+//import net.minecraft.world.entity.LivingEntity;
+//import net.minecraft.world.level.levelgen.structure.structures.WoodlandMansionPieces;
+//import org.spongepowered.asm.mixin.Mixin;
+//
+//@Mixin(WoodlandMansionPieces.WoodlandMansionPiece.class)
+//public class WoodlandMansionPiecesMixin {
+//}
